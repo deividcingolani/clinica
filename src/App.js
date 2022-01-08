9745d4b3e0d1f2b460ddc2cd747328e731260245
David <cingolanidavid@gmail.com>
2022-01-08 09:55:49 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormClinica from "./FormClinica";
+
+function App() {
+  return (
+    <div className="App">
+      <FormClinica />
+    </div>
+  );
+}
+
+export default App;
